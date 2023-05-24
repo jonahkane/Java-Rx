230b46856ffbc89b866a13db8c33cb43a41d56e8
seangibbons730 <Sean.R.Gibbons98@gmail.com>
2023-05-24 18:11:14 -0500

    changed some colors fixed something idk i forgot

--- client/src/components/DrinkForm/index.js
+++ client/src/components/DrinkForm/index.js
@@ -155,3 +155,77 @@ const DrinkForm = ({ setDrinksListCurrent }) => {
 };
 
 export default DrinkForm;
+
+// Attempted to make slide show background
+
+// (function() {
+
+//   // Settings.
+//     var settings = {
+
+//       // Images (in the format of 'url': 'alignment').
+//         images: {
+//           'https://fastly.picsum.photos/id/42/3456/2304.jpg?hmac=dhQvd1Qp19zg26MEwYMnfz34eLnGv8meGk_lFNAJR3g': 'center',
+//           'images/bg02.jpg': 'center',
+//           'images/bg03.jpg': 'center'
+//         },
+
+//       // Delay.
+//         delay: 6000
+
+//     };
+
+//   // Vars.
+//     var	pos = 0, lastPos = 0,
+//       $wrapper, $bgs = [], $bg,
+//       k, v;
+
+//   // Create BG wrapper, BGs.
+//     $wrapper = document.createElement('div');
+//       $wrapper.id = 'bg';
+//        $body.appendChild($wrapper);
+
+//     for (k in settings.images) {
+
+//       // Create BG.
+//         $bg = document.createElement('div');
+//           $bg.style.backgroundImage = 'url("' + k + '")';
+//           $bg.style.backgroundPosition = settings.images[k];
+//           $wrapper.appendChild($bg);
+
+//       // Add it to array.
+//         $bgs.push($bg);
+
+//     }
+
+//   // Main loop.
+//     $bgs[pos].classList.add('visible');
+//     $bgs[pos].classList.add('top');
+
+//     // Bail if we only have a single BG or the client doesn't support transitions.
+//       if ($bgs.length == 1
+//       ||	!canUse('transition'))
+//        return;
+
+//     window.setInterval(function() {
+
+//       lastPos = pos;
+//       pos++;
+
+//       // Wrap to beginning if necessary.
+//         if (pos >= $bgs.length)
+//           pos = 0;
+
+//       // Swap top images.
+//         $bgs[lastPos].classList.remove('top');
+//         $bgs[pos].classList.add('visible');
+//         $bgs[pos].classList.add('top');
+
+//       // Hide last image after a short delay.
+//         window.setTimeout(function() {
+//           $bgs[lastPos].classList.remove('visible');
+//         }, settings.delay / 2);
+
+//     }, settings.delay);
+
+// })();
